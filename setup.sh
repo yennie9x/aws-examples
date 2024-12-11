99b11ce863b7757c4a634d0cd6b5aa808a0e3dfc
Coder cute <yen1117823@gmail.com>
2024-12-11 23:09:29 +0700

    Update and rename create-orgs.sh to setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# Install AWS CLI v2
+pwd=$(pwd)
+cd /tmp
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+cd $pwd
+
+# Install CDK CLI
+npm install -g aws-cdk
+cdk --version
+
+# Install YARN
+npm install -g yarn
+yarn --version
+
+# Install AWS Amplify
+npm install -g @aws-amplify/cli
+
+# Install Taskfiles
+npm install -g @go-task/cli
